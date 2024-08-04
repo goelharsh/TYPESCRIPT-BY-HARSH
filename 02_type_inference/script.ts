ecf1deb76012fbadac3d759434832a29b4d4cdc8
napharsh <harsh.goel@napworks.in>
2024-08-04 23:13:51 +0530

    type inference

---
+++ 02_type_inference/script.ts
@@ -0,0 +1,9 @@
+
+// TypeScript will automatically detect variables data type, functions return type, Object types, Array Types, etc
+
+// here the type of a has been find out automatically 
+let a  = 12;
+
+// similarly for b and c 
+let b= 'a';
+let c = true;
