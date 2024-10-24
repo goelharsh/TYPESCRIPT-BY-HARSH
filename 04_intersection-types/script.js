b273fa13baf98a4444685fac452697b2fbd181ed
napharsh <harshgoel18122001@gmail.com>
2024-10-24 07:08:21 +0530

    Intersection and tsconfig

---
+++ 04_intersection-types/script.js
@@ -0,0 +1,13 @@
+// this is union, ie either number or string
+var a;
+a = 12;
+a = "harsh";
+var b = {
+    name: "harsh",
+    section: "a",
+};
+var value = {
+    name: 'yamuna nagar',
+    population: 23,
+    cities: 2
+};
--- 04_intersection-types/script.ts
+++ 04_intersection-types/script.ts
@@ -1,1 +1,47 @@
+// this is union, ie either number or string
 let a: number | string;
+a = 12;
+a = "harsh";
+
+//intersection
+// this can not happen
+// let a: number & string;
+
+// So we have to do something else
+// let b: number;
+
+// classmates is a type
+type Classmates = {
+  section: string;
+};
+type Students = {
+  name: string;
+};
+
+// now we can make intersection by joining then
+// now b should have both values 
+type ClassmatesInStudents = Classmates & Students;
+let b: ClassmatesInStudents = {
+  name: "harsh",
+  section: "a",
+};
+
+
+// Examples 
+type City = {
+    name: string,
+    population: number
+}
+
+type Planet = {
+    name: string,
+    cities: number
+}
+
+type CitiesInPlanet = City & Planet;
+let value: CitiesInPlanet = {
+    name: 'yamuna nagar',
+    population: 230000,
+    cities: 2
+}
+
